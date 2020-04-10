2dd485ab32ab50e60494d0b929883d4ee7cb5f7c
ian107x <57157594+ian107x@users.noreply.github.com>
2020-04-10 16:31:09 +0100

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+
+public class Calculator {
+    public double add(double x, double y){
+        return x + y;
+        
+    }
+    public double subtract(double x, double y){
+        return x - y;
+        
+    }
+    public double multiply(double x, double y){
+        return x*y;
+        
+    }
+    public double divide(double x, double y){
+        return x / y;
+        
+    }
+    public int modulus(int x, int y){
+        return x % y;
+        
+    }
+    public double square(double x){
+        return x * x;
+        
+    }
+    public double root(double x){
+        return (Math.sqrt(x));
+    }
+
+}
---
+++ CalculatorDriver.java
@@ -0,0 +1,14 @@
+
+public class CalculatorDriver {
+    public static void main(String[]args){
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(2, 5));
+        System.out.println(calc.subtract(2.1, 5));
+        System.out.println(calc.multiply(2, 5.4));
+        System.out.println(calc.divide(5, 2));
+        System.out.println(calc.modulus(5, 2));
+        System.out.println(calc.square(5));
+        System.out.println(calc.root(9.5));
+    }
+
+}
